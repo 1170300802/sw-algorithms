4b2b67ff556d50101ff5dce746965a25e018e6ce
swensun <1416316137@qq.com>
2018-01-26 14:22:14 +0800

    add reverse_string

---
+++ python/string_questions/reverse_string.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""
+@author: swensun
+
+@github:https://github.com/yunshuipiao
+
+@software: python
+
+@file: reverse_string.py
+
+@desc: 反转字符串
+
+@hint:
+"""
+
+def reverse_string(string):
+    #由于python的字符串不能执行  string[0] == 'a' 操作
+    result = ''
+    for i in range(len(string) - 1, -1 , -1):
+        result += string[i]
+    return result
+
+
+
+if __name__ == '__main__':
+    s = "qwertyuio"
+    print(s)
+    s = reverse_string(s)
+    print(s)
