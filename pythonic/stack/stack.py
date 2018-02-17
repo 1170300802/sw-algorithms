1f8fe960c8a0a2351194a60e873c296bc2b1aba4
swensun <1416316137@qq.com>
2018-02-17 09:39:02 +0800

    update stack

--- pythonic/stack/stack.py
+++ pythonic/stack/stack.py
@@ -31,10 +31,10 @@ class AbstractStack:
         result = "------\n"
         for element in self:
             result += str(element) + '\n'
-        return result[:-1] + '\n-----'
+        return result[:-1] + '\n------'
+
 
 class ArrayStack(AbstractStack):
-
     def __init__(self, size=10):
         AbstractStack.__init__(self)
         self.array = [None] * size
@@ -72,6 +72,46 @@ class ArrayStack(AbstractStack):
             yield self.array[probe]
             probe -= 1
 
+
+class StackNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedListStack(AbstractStack):
+    def __init__(self):
+        AbstractStack.__init__(self)
+        self.head = None
+
+    def push(self, data):
+        node = StackNode(data)
+        node.next = self.head
+        self.head = node
+        self.top += 1
+
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError("stack is empty")
+        data = self.head.data
+        self.head = self.head.next
+        self.top -= 1
+        return data
+
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError("stack is empty")
+        return self.head.data
+
+    def __iter__(self):
+        probe = self.head
+        while True:
+            if probe is None:
+                raise StopIteration
+            yield probe.data
+            probe = probe.next
+
+
 if __name__ == '__main__':
     s = ArrayStack()
     s.push(1)
@@ -80,3 +120,5 @@ if __name__ == '__main__':
     s.push(4)
     print(s.peek())
     print(s.pop())
+    for i in s:
+        print(i)
