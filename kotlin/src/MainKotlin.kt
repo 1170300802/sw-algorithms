8fb197da616f57c6bf3e5986c0b03d7e974c4054
swensun <1416316137@qq.com>
2018-05-14 10:09:51 +0800

    update fp

--- kotlin/src/MainKotlin.kt
+++ kotlin/src/MainKotlin.kt
@@ -1,13 +1,10 @@
 fun <T> T?.println() = kotlin.io.println(this)
 
 fun main(args: Array<String>) {
-    val addOne = add.partial1(1)
-    val addTwo = add.partial2("2")
-    addOne("2").println()
-    addTwo(3).println()
+    dog(Action.feed)(5)
 }
 
-val add = {a:Int, b: String -> "$a$b"}
+val add = { a: Int, b: String -> "$a$b" }
 
 fun <P1, P2, P3, R> Function3<P1, P2, P3, R>.curried()
         = fun(p1: P1) = fun(p2: P2) = fun(p3: P3) = this(p1, p2, p3)
@@ -23,7 +20,17 @@ fun <P1, P2, R> Function2<P1, P2, R>.partial1(p1: P1) = fun(p2: P2) = this(p1, p
 fun <P1, P2, R> Function2<P1, P2, R>.partial2(p2: P2) = fun(p1: P1) = this(p1, p2)
 
 
+val dog = { action: Action ->
+    var weight = 10
+    when (action) {
+        Action.feed -> { food: Int -> weight += food; println(weight) }
+        Action.workout -> { intensity: Int -> weight += intensity; println(weight) }
+    }
+}
 
+enum class Action {
+    feed, workout
+}
 
 
 
